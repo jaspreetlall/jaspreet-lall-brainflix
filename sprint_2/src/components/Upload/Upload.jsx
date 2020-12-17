abe35c1816dc309aee7c2a0a10421ed1a512c9cd
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2020-12-16 21:00:56 -0800

    Added HTML structure for upload page.

--- sprint_2/src/components/Upload/Upload.jsx
+++ sprint_2/src/components/Upload/Upload.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link} from 'react-router-dom';
+import VideoPreview from '../../assets/images/Upload-video-preview.jpg';
 
 import Header from '../Header/Header';
 
@@ -6,6 +8,48 @@ export default function Upload() {
   return (
     <div>
       <Header />
+
+      <h1>Upload Video</h1>
+      <form>
+
+        <div>
+
+          <div>
+            <h4>Video Thumbnail</h4>
+            <div>
+              <img src={VideoPreview} alt="Bicycle handle bar in focus"/>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <label htmlFor="title">Title your video</label>
+              <input
+                className=""
+                type="text"
+                id="title"
+                name="title"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="description">Add a video description</label>
+              <input
+                className=""
+                type="text"
+                id="description"
+                name="description"
+              />
+            </div>
+          </div>
+
+        </div>
+
+        <div>
+          <button>Publish</button>
+          <Link to="/">Cancel</Link>
+        </div>
+      </form>
     </div>
   )
 }
