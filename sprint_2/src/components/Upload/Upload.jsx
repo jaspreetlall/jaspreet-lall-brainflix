d13f62203df3ba11f68eced49dddf83578f7024d
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2020-12-17 22:33:38 -0800

    Styling for upload page mobile layout complete. Incorporated feedback.

--- sprint_2/src/components/Upload/Upload.jsx
+++ sprint_2/src/components/Upload/Upload.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import './Upload.scss';
 import VideoPreview from '../../assets/images/Upload-video-preview.jpg';
 
 import Header from '../Header/Header';
@@ -9,47 +10,51 @@ export default function Upload() {
     <div>
       <Header />
 
-      <h1>Upload Video</h1>
-      <form>
+      <section className="upload container">
+        <h1 className="upload__head">Upload Video</h1>
+        <form className="upload__form">
 
-        <div>
+          <div className="upload__form-detail">
 
-          <div>
-            <h4>Video Thumbnail</h4>
-            <div>
-              <img src={VideoPreview} alt="Bicycle handle bar in focus"/>
+            <div className="upload__form-detail-preview">
+              <h4 className="upload__form-detail-preview-title">Video Thumbnail</h4>
+              <div className="upload__form-detail-preview-wrapper">
+                <img className="upload__form-detail-preview-wrapper-img" src={VideoPreview} alt="Bicycle handle bar in focus"/>
+              </div>
             </div>
+
+            <div className="upload__form-detail-input">
+              <div className="upload__form-detail-input-block">
+                <label className="upload__form-detail-input-block-label" htmlFor="title">Title your video</label>
+                <input
+                  className="upload__form-detail-input-block-field upload__form-detail-input-block-field--title"
+                  type="text"
+                  id="title"
+                  name="title"
+                  placeholder="Add a title to your video"
+                />
+              </div>
+
+              <div className="upload__form-detail-input-block">
+                <label className="upload__form-detail-input-block-label" htmlFor="description">Add a video description</label>
+                <textarea
+                  className="upload__form-detail-input-block-field upload__form-detail-input-block-field--desc"
+                  type="text"
+                  id="description"
+                  name="description"
+                  placeholder="Add a description of your video"
+                />
+              </div>
+            </div>
+
           </div>
 
-          <div>
-            <div>
-              <label htmlFor="title">Title your video</label>
-              <input
-                className=""
-                type="text"
-                id="title"
-                name="title"
-              />
-            </div>
-
-            <div>
-              <label htmlFor="description">Add a video description</label>
-              <input
-                className=""
-                type="text"
-                id="description"
-                name="description"
-              />
-            </div>
+          <div className="upload__form-action">
+            <button className="upload__form-action-button upload__form-action-button--publish">Publish</button>
+            <Link className="upload__form-action-button upload__form-action-button--cancel" to="/">Cancel</Link>
           </div>
-
-        </div>
-
-        <div>
-          <button>Publish</button>
-          <Link to="/">Cancel</Link>
-        </div>
-      </form>
+        </form>
+      </section>
     </div>
   )
 }
